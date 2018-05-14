2de4fd8e2740fe02d28f6a41afd09b73f12f55f7
yidinghe <yidinghe2012@gmail.com>
2018-05-14 17:00:51 -0400

    Finished add Pointer to Ar fragment.

--- app/src/main/java/com/yidinghe/arcoresample/MainActivity.kt
+++ app/src/main/java/com/yidinghe/arcoresample/MainActivity.kt
@@ -1,15 +1,23 @@
 package com.yidinghe.arcoresample
 
+import android.graphics.Point
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import com.google.ar.core.Plane
+import com.google.ar.core.TrackingState
 import com.google.ar.sceneform.ux.ArFragment
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 private var fragment: ArFragment? = null
+
+private val pointer = PointerDrawable()
+private var isTracking = false
+private var isHitting = false
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +27,60 @@ class MainActivity : AppCompatActivity() {
 
         fragment = supportFragmentManager.findFragmentById(R.id.sceneform_fragment) as ArFragment
 
+        fragment?.let {
+            it.arSceneView.scene.setOnUpdateListener { frameTime ->
+                it.onUpdate(frameTime)
+                this.onUpdate()
+            }
+        }
+    }
+
+    private fun onUpdate() {
+        val trackingChanged = updateTracking()
+        val contentView = findViewById<View>(android.R.id.content)
+        if (trackingChanged) {
+            if (isTracking) {
+                contentView.overlay.add(pointer)
+            } else {
+                contentView.overlay.remove(pointer)
+            }
+            contentView.invalidate()
+        }
+
+        if (isTracking) {
+            val hitTestChanged = updateHitTest()
+            if (hitTestChanged) {
+                pointer.setEnabled(isHitting)
+                contentView.invalidate()
+            }
+        }
+    }
+
+    private fun updateHitTest(): Boolean {
+        val frame = fragment!!.arSceneView.arFrame
+        val pt = getScreenCenter()
+        val wasHitting = isHitting
+        isHitting = false
+        val hits = frame.hitTest(pt.x.toFloat(), pt.y.toFloat())
+        hits.forEach {
+            val trackable = it.trackable
+            if (trackable is Plane && trackable.isPoseInPolygon(it.hitPose)) {
+                isHitting = true
+            }
+        }
+        return wasHitting != isHitting
+    }
+
+    private fun updateTracking(): Boolean {
+        val frame = fragment!!.arSceneView.arFrame
+        val wasTracking = isTracking
+        isTracking = frame.camera.trackingState == TrackingState.TRACKING
+        return isTracking != wasTracking
+    }
+
+    private fun getScreenCenter(): Point {
+        val view = findViewById<View>(android.R.id.content)
+        return Point(view.width / 2, view.height / 2)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
